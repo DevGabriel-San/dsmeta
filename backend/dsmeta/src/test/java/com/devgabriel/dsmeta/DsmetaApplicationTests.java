c611e237c063eb28e2e3351ea6acc7df82f49c77
Dev-GabrielSan <gabriel.ssantos@bandtec.com.br>
2022-11-14 11:20:56 -0300

    Projeto criado

---
+++ backend/dsmeta/src/test/java/com/devgabriel/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devgabriel.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

aa102d9f717d86dfb417fff3828e3c47242baa66
Dev-GabrielSan <gabriel.ssantos@bandtec.com.br>
2022-11-14 13:33:40 -0300

    Outros componentes

--- fronted/src/App.tsx
+++ fronted/src/App.tsx
@@ -1,15 +1,20 @@
-import NoticationButton from "./components/NotificationButton";
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
+
 
 function App() {
- return ( 
-  <>
-  <h1>Olá Dev</h1>
-  <NoticationButton/>
-  <NoticationButton/><NoticationButton/><NoticationButton/>
- 
-
-  </>
- )
+    return (
+        <>
+            <Header />
+            <main>
+                <section id="sales">
+                    <div className="dsmeta-container">
+                        <SalesCard />
+                    </div>
+                </section>
+            </main>
+        </>
+    )
 }
 
 export default App

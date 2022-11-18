7303b7248f7a2203f3da18e38c2f515285dd6149
Dev-GabrielSan <gabriel.ssantos@bandtec.com.br>
2022-11-18 18:14:36 -0300

    Integração com api Twilo sms

--- backend/dsmeta/src/main/java/com/devgabriel/dsmeta/controllers/SaleController.java
+++ backend/dsmeta/src/main/java/com/devgabriel/dsmeta/controllers/SaleController.java
@@ -2,12 +2,10 @@ package com.devgabriel.dsmeta.controllers;
 
 import com.devgabriel.dsmeta.entities.Sale;
 import com.devgabriel.dsmeta.service.SaleService;
+import com.devgabriel.dsmeta.service.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.data.domain.Pageable;
 import java.util.List;
@@ -18,12 +16,20 @@ public class SaleController {
     @Autowired
    public SaleService service;
 
+    @Autowired
+    public SmsService smsService;
+
     @GetMapping
     public Page<Sale> findSales(
             @RequestParam(value="minDate", defaultValue = "")String minDate,
             @RequestParam(value="maxDate", defaultValue = "")String maxDate,
             Pageable pageable){
         return service.findSales(minDate,maxDate, pageable);
+    }
+
+    @GetMapping("{id}/notification")
+    public void notSms(@PathVariable Long id){
+        smsService.sendSms(id);
     }
 
 }
